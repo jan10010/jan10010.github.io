6474c113c5719054d6a9115ed524da27c5e3aaed
jan10010 <jankoelling@outlook.de>
2021-03-10 14:56:41 +0000

    Daly data update 2

--- dalyUpdate.sh
+++ dalyUpdate.sh
@@ -3,6 +3,13 @@ echo "starting script"
 echo $PATH
 
 node ./code/updateData.js
+echo "download complite"
+cd geojsonMap
+echo "Starting compression"
+mapshaper -i ./RKI_Corona_Landkreise.geojson -o precision=0.001 -simplify dp 10% -o format=geojson -o ./RKI_Corona_Landkreise.geojson
+echo "compression complite"
+cd ..
+echo "Starting upload"
 git add --all
 git commit -m  "Daly data update 2"
 git push origin master
